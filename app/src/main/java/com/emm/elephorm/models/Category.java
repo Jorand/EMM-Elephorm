f9cf810af64492058652701886574d7083b93c1b
Inikaam <inikaam@gmail.com>
2015-08-12 13:00:23 +0200

    Objets Category et Subcategory avec les getters

--- app/src/main/java/com/emm/elephorm/models/Category.java
+++ app/src/main/java/com/emm/elephorm/models/Category.java
@@ -78,4 +78,22 @@ public class Category {
 
         return categories;
     }
+
+    /** Getters **/
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public List<Subcategory> getSubcategories() {
+        return subcategories;
+    }
 }
--- app/src/main/java/com/emm/elephorm/models/Subcategory.java
+++ app/src/main/java/com/emm/elephorm/models/Subcategory.java
@@ -27,4 +27,22 @@ public class Subcategory {
         }
 
     }
+
+    /** Getters **/
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
 }
